c90fc40bc46bf7c31f6e2309ad3c5535909168c2
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-10-25 20:15:30 +0530

    done

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import assets from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
+const Sidebar = ({seletedUser,setSelectedUser}) => {
+    const navigate = useNavigate();
+  return (
+    <div>
+      <div className='pb-5'>
+        <div className='flex justify-between item-center'>
+            <img src={assets.logo} alt="logo" className='max-w-40' />
+            <div className="relative py-2 group">
+                <img src={assets.menu_icon} alt="menu" className='max-h-5 cursor-pointer' />
+                <div>
+                    <p  onClick={()=>navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
+                    <hr className="my-2 border-t border-gray-500" />
+                    <p className='cursor-pointer text-sm'> Logout</p>
+                </div>
+
+            </div>
+
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
