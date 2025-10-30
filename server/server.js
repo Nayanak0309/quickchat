19acc1734a8c9f957291da0dcfd49ea3dc88c079
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-10-30 13:11:30 +0530

    server connection

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 import express from "express";
-import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
-//Create Express app and HTTP server//
+dotenv.config(); // Make sure this runs BEFORE connectDB()
+
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
-//middleware setup//
-
-app.use(express.json({limit: "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-app.use("/api/status", (req, res)=> res.send("Server is live"));
+app.get("/api/status", (req, res) => res.send("Server is live"));
 
-const PORT= process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("server is running on PORT: " + PORT));
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log("✅ Server running on PORT: " + PORT));
