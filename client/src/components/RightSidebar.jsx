69cb71f7d6cfd5493de058de38cba7126416bcfd
Nayanak0309 <nayanaramanth2003@gmail.com>
2025-11-08 21:28:02 +0530

    done

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect} from 'react'
+import  {useRef, useEffect} from 'react'
 import assets, { imagesDummyData } from '../assets/assets/assets'
 
 const RightSidebar = ({selectedUser}) => {
